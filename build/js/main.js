f04d7febcf13dcb11f7678fb56d71ba2ff6f847e
Odo Maurice{alphaCodes} <92038525+odomaurice@users.noreply.github.com>
2022-12-26 22:49:53 +0100

    Add files via upload

--- build/js/main.js
+++ build/js/main.js
@@ -13,19 +13,19 @@ const initApp = () => {
 };
 
 // <!-- typed js effect starts -->
-  var typed = new Typed(".typing-text", {
-    strings: [
-      "33,100 Community",
-      "5,500  Happy Clients",
-      "180 Jobs and University Partners",
-      "11,000 Oppurtunities Shared",
-      "And so much more.....",
-    ],
-    loop: true,
-    typeSpeed: 50,
-    backSpeed: 25,
-    backDelay: 500,
-  });
+  // var typed = new Typed(".typing-text", {
+  //   strings: [
+  //     "33,100 Community",
+  //     "5,500  Happy Clients",
+  //     "180 Jobs and University Partners",
+  //     "11,000 Oppurtunities Shared",
+  //     "And so much more.....",
+  //   ],
+  //   loop: true,
+  //   typeSpeed: 50,
+  //   backSpeed: 25,
+  //   backDelay: 500,
+  // });
   // <!-- typed js effect ends -->
 
   
