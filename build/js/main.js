a2084a3b6bff680f0845cc40b89523e27c0c6431
Odo Maurice{alphaCodes} <92038525+odomaurice@users.noreply.github.com>
2022-12-29 11:41:34 +0100

    Add files via upload
    update on the navbar, added search bar and updated the footer section

--- build/js/main.js
+++ build/js/main.js
@@ -1,16 +1,22 @@
-const initApp = () => {
-  const hamburgerBtn = document.getElementById("hamburger-button");
-  const mobileMenu = document.getElementById("mobile-menu");
+// const initApp = () => {
+//   const hamburgerBtn = document.getElementById("hamburger-button");
+//   const mobileMenu = document.getElementById("mobile-menu");
+ 
 
-  const toggleMenu = () => {
-    mobileMenu.classList.toggle("hidden");
-    mobileMenu.classList.toggle("flex");
-    hamburgerBtn.classList.toggle("toggle-btn");
-  };
+//   const toggleMenu = () => {
+//     mobileMenu.classList.toggle("hidden");
+//     mobileMenu.classList.toggle("flex");
+//     hamburgerBtn.classList.toggle("toggle-btn");
+   
+//   };
 
-  hamburgerBtn.addEventListener("click", toggleMenu);
-  mobileMenu.addEventListener("click", toggleMenu);
-};
+//   hamburgerBtn.addEventListener("click", toggleMenu);
+//   mobileMenu.addEventListener("click", toggleMenu);
+  
+// };
+
+
+
 
 // <!-- typed js effect starts -->
   // var typed = new Typed(".typing-text", {
@@ -28,48 +34,6 @@ const initApp = () => {
   // });
   // <!-- typed js effect ends -->
 
-  /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-
-
-        function myFunction2() {
-
-            document.getElementById("myDropdown2").classList.toggle("show");
-        }
-
-        // Close the dropdown menu if the user clicks outside of it
-        window.onclick = function (event) {
-            if (!event.target.matches('.dropbtn')) {
-                var dropdowns = document.getElementsByClassName("dropdown-content");
-                var i;
-                for (i = 0; i < dropdowns.length; i++) {
-                    var openDropdown = dropdowns[i]; if (openDropdown.classList.contains('show')) {
-                        openDropdown.classList.remove('show');
-                    }
-                }
-            }
-        }
-   
-
   
-
      
-document.addEventListener("DOMContentLoaded", initApp);
+// document.addEventListener("DOMContentLoaded", initApp);
